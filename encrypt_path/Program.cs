b7c7764f54c8ef37658d9f299ecf0612cbe1c59f
Caleb Anderson <Locutus83@users.noreply.github.com>
2016-08-18 14:06:41 -0600

    Code v1
    Here is the first version of the completed code.

---
+++ encrypt_path/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace encrypt_path
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //String MyPath = "C:\\Enter\\Path\\Here";
+                Console.WriteLine("Please enter your path here:");
+                String MyPath = Console.ReadLine();
+                String Encrypted = PIDLUtils.Encode(MyPath);
+                Console.WriteLine(Encrypted);
+
+                String notEncrypted = PIDLUtils.Decode(Encrypted);
+                Console.WriteLine(notEncrypted);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
